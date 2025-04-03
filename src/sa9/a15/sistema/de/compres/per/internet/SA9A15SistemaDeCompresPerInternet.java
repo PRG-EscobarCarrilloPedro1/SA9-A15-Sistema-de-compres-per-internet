b5f2ea2631ce58d7195522b0968241d7c063bcf7
Ghenesys00 <exe@example.com>
2025-04-03 18:43:15 +0200

    [ADD] Añadido principio

---
+++ src/sa9/a15/sistema/de/compres/per/internet/SA9A15SistemaDeCompresPerInternet.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sa9.a15.sistema.de.compres.per.internet;
+
+/**
+ *
+ * @author pedro
+ */
+public class SA9A15SistemaDeCompresPerInternet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
